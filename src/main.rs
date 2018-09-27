8157f2e873421c7330a3f1add81ad194521855ad
lars <lars@kloud.software>
2018-09-27 20:22:55 +0200

    demo progress

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,69 @@
 extern crate rand;
-extern crate ngalgebra;
+
+#[macro_use(array)]
+extern crate ndarray;
+use ndarray::Array1;
+use ndarray::Array2;
 
 fn main() {
-    println!("Hello, world!");
+    let mut neural_network = NeuralNetwork::new();
+    
+    let inputs = array![[0.,0.,1.],
+                        [1.,1.,1.],
+                        [1.,0.,1.],
+                        [0.,1.,1.]];
+
+    let outputs = array![0., 1., 1., 0.];
+
+    let unknown = array![1., 0., 0.];
+
+    neural_network.train(&inputs, &outputs, 10000);
+
+    print!("{}", neural_network.think_1_dim(&unknown));
+
+
 }
 
 struct NeuralNetwork {
-    weights: Vec<f64>,
+    weights: Array1<f64>,
 }
 
 impl NeuralNetwork {
-    pub fn new() -> NeuralNetwork{
-        let mut network = NeuralNetwork {weights: Vec::new()};
-        network.weights.push(rand::random::<f64>());
-        network.weights.push(rand::random::<f64>());
-        network.weights.push(rand::random::<f64>());
+    pub fn new() -> NeuralNetwork {
+        let mut network = NeuralNetwork {weights: array![rand::random::<f64>(), rand::random::<f64>(), rand::random::<f64>()]};
         network
     }
 
-    pub fn train(&self, training_set_inputs: &[f64], training_set_outputs: &[f64]) {
-        let output = &self.think(&training_set_inputs);
+    pub fn train(&mut self, training_set_inputs: &Array2<f64>, training_set_outputs: &Array1<f64>, iter: i32) {
+        for i in (1..iter) {
+            let output = &self.think(&training_set_inputs);
 
-        let error = training_set_outputs - output;
+            let error = training_set_outputs - output;
 
-        let adjustment = dot()
+            let adjustment = training_set_inputs.t().dot(&(error * &derived_sigmoid(output)));
+
+            self.weights = &self.weights + &adjustment;
+        }
+        
     }
 
-    fn think(&self, inputs: &[f64]) -> f64 { 
-        sigmoid(dot(&inputs, &self.weights))
+    pub fn think(&self,inputs: &Array2<f64>) -> Array1<f64> { 
+        sigmoid(&inputs.dot(&self.weights))
+    }
+
+    pub fn think_1_dim(&self, inputs: &Array1<f64>) -> Array1<f64> { 
+        sigmoid(&array![inputs.dot(&self.weights)])
     }
 }
 
-fn sigmoid(x: i64) -> f64 {
-    1f64 / (1f64 + f64::powf(std::f64::consts::E, -x as f64))
+fn sigmoid(x: &Array1<f64>) -> Array1<f64> {
+    matrix_pow_e(x).map(|y| y + 1.).map(|val| 1. / val)
 }
 
-fn derived_sigmoid(x: i64) -> i64 {
-    x * (1 - x)
+fn derived_sigmoid(matrix: &Array1<f64>) -> Array1<f64> {
+    matrix.map(|x| x * (1. - x))
 }
 
+fn matrix_pow_e(matrix: &Array1<f64>) -> Array1<f64> {
+    matrix.map(|x| std::f64::consts::E.powf(-x))
+}
